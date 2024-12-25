5d6a9a9a69b8073da5434619d084e7532ba7eba2
violasptntels <violaseptiantiana23@gmail.com>
2024-12-25 14:52:52 +0700

    tambah header transparan

--- js/home.js
+++ js/home.js
@@ -57,3 +57,13 @@ function createQRCode(container, value) {
     qrCanvas = qr.canvas;
     container.appendChild(qr.canvas);
 }
+
+// Script untuk mengubah header saat di-scroll
+document.addEventListener("scroll", function () {
+    const header = document.getElementById("header");
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
